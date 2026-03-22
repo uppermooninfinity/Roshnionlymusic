b36ff394def68acd02d76b7da7a81a6483b95f0a
˹ 🎧 ᴛᴇᴀᴍ ɪɴғɪɴɪᴛʏ ʙᴏᴛs ˼ <networksmite@gmail.com>
2026-03-22 19:09:42 +0530

    Update help.py

--- Oneforall/utils/inline/help.py
+++ Oneforall/utils/inline/help.py
@@ -1,8 +1,8 @@
 from typing import Union
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from pyrogram.enums imporrt ButtonStyle
-k
+from pyrogram.enums import ButtonStyle
+
 from Oneforall import app
 
     first = [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")]
