44517894f750bb97fc7cfd5ffb308e8b71b55df2
˹ 🎧 ᴛᴇᴀᴍ ɪɴғɪɴɪᴛʏ ʙᴏᴛs ˼ <networksmite@gmail.com>
2026-03-23 16:24:02 +0530

    Update skip.py

--- Oneforall/plugins/admins/skip.py
+++ Oneforall/plugins/admins/skip.py
@@ -10,7 +10,7 @@ from Oneforall.utils.database import get_loop
 from Oneforall.utils.decorators import AdminRightsCheck
 from Oneforall.utils.inline import close_markup, stream_markup, stream_markup2
 from Oneforall.utils.stream.autoclear import auto_clean
-from Oneforall.utils.thumbnails import gen_thumb
+from Oneforall.utils.thumbnails import get_thumb
 
 
 @app.on_message(
@@ -115,7 +115,7 @@ async def skip(cli, message: Message, _, chat_id):
         except:
             return await message.reply_text(_["call_6"])
         button = stream_markup2(_, chat_id)
-        img = await gen_thumb(videoid)
+        img = await get_thumb(videoid)
         run = await message.reply_photo(
             photo=img,
             caption=_["stream_1"].format(
@@ -148,7 +148,7 @@ async def skip(cli, message: Message, _, chat_id):
         except:
             return await mystic.edit_text(_["call_6"])
         button = stream_markup(_, videoid, chat_id)
-        img = await gen_thumb(videoid)
+        img = await get_thumb(videoid)
         run = await message.reply_photo(
             photo=img,
             caption=_["stream_1"].format(
@@ -221,7 +221,7 @@ async def skip(cli, message: Message, _, chat_id):
             db[chat_id][0]["markup"] = "tg"
         else:
             button = stream_markup(_, videoid, chat_id)
-            img = await gen_thumb(videoid)
+            img = await get_thumb(videoid)
             run = await message.reply_photo(
                 photo=img,
                 caption=_["stream_1"].format(
